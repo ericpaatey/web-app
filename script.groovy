9b9e42fac0de9041747c9bcb08c42f956f40e499
Eric Paatey <147304380+ericpaatey@users.noreply.github.com>
2026-03-01 03:00:03 -0600

    Enhance deployImage function for Docker deployment
    Added deployment functionality using Docker and SSH.

--- script.groovy
+++ script.groovy
@@ -21,6 +21,13 @@ def buildImage() {
 
 def deployImage() {
     echo"Deploying the application..."
+    script {
+         def dockerCmd = 'docker run -p 3080:3080 -d ericpaatey/web-apps:1.0'
+         sshagent() {
+              ssh "ssh -o StrictHostKeyChecking=no ec2-user@18.184.54.160 ${dockerCmd}"
+         }
+    } 
+     
 }
 
 return this
